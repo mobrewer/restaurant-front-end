da57b3827fd811931bd293bceb0602e071701201
esaltzm <elisaltzman@gmail.com>
2022-12-05 10:14:53 -0500

    added restaurants list with sort by price

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import restaurantData from '../restaurants.json'
 
 export default function RestaurantsList() {
+    const [sortParam, setSortParam] = useState(null)
+    const [restaurants, setRestaurants] = useState(restaurantData)
+
+    useEffect(() => {
+        let sortedRestaurants
+        sortParam === 'low'
+            ? sortedRestaurants = restaurants.sort((a, b) => b.priceRange - a.priceRange)
+            : sortedRestaurants = restaurants.sort((a, b) => a.priceRange - b.priceRange)
+        setRestaurants(sortedRestaurants)
+    }, [sortParam])
+
     return (
-        <div>
-            List of all restaurants
-        </div>
+        <>
+            <h2>Your Restaurants:</h2>
+            <select name="sortParam" onChange={(e) => setSortParam(e.target.value)}>
+                <option value="none" selected disabled hidden>Sort by price:</option>
+                <option value="low">{`Price (low to high)`}</option>
+                <option value="high">{`Price (high to low)`}</option>
+            </select>
+            <ul>
+                {restaurants.map(restaurant => <li>{restaurant.name} - {restaurant.cuisine} - {'$'.repeat(restaurant.priceRange)}</li>)}
+            </ul>
+        </>
     )
 }
